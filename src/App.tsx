6aecadce3e08230cb0a501e7dc1a8ed5826e782e
Luiz Fellipe <l.silva@ainda.ai>
2023-05-12 14:43:43 -0300

    chore: add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { RouterProvider } from "react-router-dom"
-import { ThemeProvider } from "styled-components"
-import router from "./routes"
+import { RouterProvider } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import router from './routes';
 
 //STYLES
-import { defaultTheme } from "./styles/themes/default"
-import { GlobalStyle } from "./styles/global"
+import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/global';
 
 function App() {
   return (
-   <ThemeProvider theme={defaultTheme}>
+    <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
       <RouterProvider router={router} />
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
